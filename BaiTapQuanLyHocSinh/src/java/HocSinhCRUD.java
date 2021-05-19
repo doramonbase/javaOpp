31ca2e69762ab06467ad4a3e399feefc534bf1fc
The VU <doramonbase@gmail.com>
2021-05-19 13:33:11 +0700

    update

--- BaiTapQuanLyHocSinh/src/java/HocSinhCRUD.java
+++ BaiTapQuanLyHocSinh/src/java/HocSinhCRUD.java
@@ -16,12 +16,20 @@ import java.util.List;
  * @author admin
  */
 public class HocSinhCRUD {
-    String db_url = "jdbc:mysql://localhost/phpmyadmin/db_structure.php?server=1&db=quan_ly_hoc_sinh";
-    String user_name = "root";
-    String pass = "";
+    String db_url;
+    String user_name;
+    String pass;
     Connection conn;
+
+    public HocSinhCRUD(String db_url, String user_name, String pass) {
+        this.db_url = db_url;
+        this.user_name = user_name;
+        this.pass = pass;
+    }
+
     
-    void connect() throws SQLException{
+    
+    final void connect() throws SQLException{
         if (conn ==  null || conn.isClosed()) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
@@ -32,25 +40,120 @@ public class HocSinhCRUD {
         }
     }
     
-    void disconnect() throws SQLException {
+    final void disconnect() throws SQLException {
         if (conn != null && !conn.isClosed()) {
             conn.close();
         }
     }
     
-    public boolean insertStudent(Student student) throws SQLException {
+    public List<HocSinh> studentList() throws SQLException {
+        List<HocSinh> listStudent;
+        listStudent = new ArrayList<>();
+        String sql = "SELECT * FROM student";
+        connect();
+        
+        try (Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(sql)) {
+            
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String address = rs.getString("address");
+                float markC = rs.getFloat("markC");
+                float markPHP = rs.getFloat("markPHP");
+                
+                HocSinh student = new HocSinh(id, name, email, address, markC, markPHP);
+                listStudent.add((HocSinh)student);
+            }
+            rs.close();
+            statement.close();
+        }
+        
+        disconnect();
+        return listStudent;
+        
+        
+    }
+    
+    public boolean insertStudent(HocSinh student) throws SQLException {
         String sql = "INSERT INTO student values (null, ?, ?, ?, ?, ?)";
         connect();
         
-        PreparedStatement statement = conn.prepareStatement(sql);
-        statement.setString(1, student.getName());
-        statement.setString(2, student.getEmail());
-        statement.setString(3, student.getAddress());
-        statement.setFLoat(4, student.getMarkC());
-        statement.setFloat(5, student.getMarkPHP());
+        boolean rs;
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setString(1, student.getName());
+            statement.setString(2, student.getEmail());
+            statement.setString(3, student.getAddress());
+            statement.setFloat(4, student.getMarkC());
+            statement.setFloat(5, student.getMarkPHP());
+            rs = statement.executeUpdate() > 0;
+            
+            statement.close();
+        }
         
-        boolean rs = statement.executeUpdate() > 0;
-        statement.close();
         disconnect();
         return rs;
     }
+    
+    public boolean updateStudent(HocSinh student) throws SQLException {
+        String sql = "UPDATE student SET name = ?, email = ?, address = ?, markC = ?, markPHP = ? WHERE id = ?";
+        connect();
+        
+        boolean rs;
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setString(1, student.getName());
+            statement.setString(2, student.getEmail());
+            statement.setString(3, student.getAddress());
+            statement.setFloat(4, student.getMarkC());
+            statement.setFloat(5, student.getMarkPHP());
+            statement.setInt(6, student.getId());
+            rs = statement.executeUpdate() > 0;
+            
+            statement.close();
+        }
+        disconnect();
+        return rs;
+    } 
+    
+    public boolean deleteStudent(HocSinh student) throws SQLException {
+        String sql = "DELETE FROM student WHERE id = ?";
+        connect();
+        
+        boolean rs;
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setInt(1, student.getId());
+            rs = statement.executeUpdate() > 0;
+            
+            statement.close();
+        }
+        disconnect();
+        return rs;
+    } 
+    
+    public HocSinh findStudent(int id) throws SQLException {
+        HocSinh student = null;
+        String sql = "SELECT * FROM student WHERE id = ?";
+        
+        connect();
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            try (ResultSet rs = statement.executeQuery()) {
+                if(rs.next()) {
+                    int idStudent = rs.getInt("id");
+                    String name = rs.getString("name");
+                    String email = rs.getString("email");
+                    String address = rs.getString("address");
+                    float markC = rs.getFloat("markC");
+                    float markPHP = rs.getFloat("markPHP");
+                    
+                    student = new HocSinh(idStudent, name, email, address, markC, markPHP);
+                    
+                }
+            }
+            statement.close();
+        }
+        return student;
+    }
+}
+    
